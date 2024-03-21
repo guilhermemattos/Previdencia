48f60775c8844a8747d8a2bcf58efe731c319600
guilhermemattos <guilherme.lial@gmail.com>
2024-03-21 00:13:09 -0300

    Incluindo os repositories

---
+++ Previdencia/Repositories/IndicesRepository.cs
@@ -0,0 +1,17 @@
+﻿using Previdencia.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Previdencia.Repositories
+{
+    public class IndicesRepository
+    {
+        public void Inserir(Indices indices)
+        {
+
+        }
+    }
+}
---
+++ Previdencia/Repositories/ProdutoRepository.cs
@@ -0,0 +1,17 @@
+﻿using Previdencia.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Previdencia.Repositories
+{
+    public class ProdutoRepository
+    {
+        public void Inserir(Produto produto) 
+        {
+            
+        }
+    }
+}
