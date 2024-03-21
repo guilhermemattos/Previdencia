09c51424c1c288a35bdb41f60c8df4b3d2993fb4
guilhermemattos <guilherme.lial@gmail.com>
2024-03-21 00:18:58 -0300

    Inclusão do AppSettings

---
+++ Previdencia/Settings/AppSettings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Previdencia.Settings
+{
+    public class AppSettings
+    {
+        public static string ConnectionString
+            => "Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=Previdencia;Integrated Security=True;";
+    }
+}
